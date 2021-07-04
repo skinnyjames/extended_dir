760708a7f87b8b0d6fb8a5602c1a22aaae3bdf64
Sean Gregory <sean.christopher.gregory@gmail.com>
2021-07-04 13:17:29 -0400

    remove unneeded iteration

--- lib/extended_dir.rb
+++ lib/extended_dir.rb
@@ -1,9 +1,5 @@
 module ArrayRefinement
   refine Array do
-    def with_ruby_extension
-      select { |member| member =~ /\.rb$/ }
-    end
-
     def sort_by_with_index!
       sort_by! do |member|
         yield member, index(member)
@@ -15,22 +11,23 @@ end
 class ExtendedDir
   using ArrayRefinement
 
-  def self.entries(path, order: [])
+  def self.files(path, order: [])
     sort_files(path, order)
   end
 
   def self.require_all(path, order: [])
-    sort_files(path, order).each { |file| require file }
+    sort_files(path, order, is_ruby: true).each { |file| require file }
   end
 
   def self.load_all(path, order: [])
-    sort_files(path, order).each { |file| load file }
+    sort_files(path, order, is_ruby: true).each { |file| load file }
   end
 
   private
 
-  def self.sort_files(path, order)
-    files = Dir.glob("#{path}/**/*").with_ruby_extension
+  def self.sort_files(path, order, is_ruby: false)
+    pattern = is_ruby ? "#{path}/**/*.rb" : "#{path}/**/*.*"
+    files = Dir.glob(pattern)
     files.sort_by_with_index! do |file, i|
       index = order.find_index do |ordered_file|
         entries = Dir.glob("#{path}/#{ordered_file}")
--- spec/extended_dir_spec.rb
+++ spec/extended_dir_spec.rb
@@ -1,5 +1,4 @@
 describe ExtendedDir do
-  
   it "should load in default order" do 
     ExtendedDir.load_all("#{__dir__}/fixtures")
     expect(Fixture.value).to eql("a")
@@ -7,6 +6,16 @@ describe ExtendedDir do
 
   it "should load in a specified order" do 
     ExtendedDir.load_all("#{__dir__}/fixtures", order: ['a.rb'])
+    expect(Fixture.value).to eql("c")
+  end
+
+  it "should load globs" do 
+    ExtendedDir.load_all("#{__dir__}/fixtures", order: ["1/c.??", "a.rb"])
     expect(Fixture.value).to eql("b")
+  end
+
+  it "should load dir globs" do 
+    ExtendedDir.load_all("#{__dir__}/fixtures", order: ["a.rb", "1/*.rb"])
+    expect(Fixture.value).to eql("d")
   end
 end
